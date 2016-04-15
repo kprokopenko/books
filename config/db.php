ca92aaa3caee73eb2805551d7152ef7cfb983db4
prokopenko <prokopenko@skytracking.ru>
2016-04-15 10:58:56 +0300

    Конфиги для БД

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=db;dbname=books',
+    'username' => 'books',
+    'password' => 'secret',
     'charset' => 'utf8',
 ];
