7e6eee8a4a351d5f04a92ad96f13875dc37d830b
prokopenko <prokopenko@skytracking.ru>
2016-04-15 11:35:39 +0300

    Тестовые данные

---
+++ tests/codeception/unit/fixtures/data/authors.php
@@ -0,0 +1,44 @@
+<?php
+
+return [
+    [
+        'first_name' => 'Ян',
+        'last_name' => 'Большаков',
+    ],
+    [
+        'first_name' => 'Искра',
+        'last_name' => 'Устинов',
+    ],
+    [
+        'first_name' => 'Филипп',
+        'last_name' => 'Михайлов',
+    ],
+    [
+        'first_name' => 'Клим',
+        'last_name' => 'Аксёнов',
+    ],
+    [
+        'first_name' => 'Вадим',
+        'last_name' => 'Афанасьев',
+    ],
+    [
+        'first_name' => 'Инга',
+        'last_name' => 'Самсонов',
+    ],
+    [
+        'first_name' => 'Анжелика',
+        'last_name' => 'Аксёнов',
+    ],
+    [
+        'first_name' => 'Зоя',
+        'last_name' => 'Нестеров',
+    ],
+    [
+        'first_name' => 'Мальвина',
+        'last_name' => 'Григорьев',
+    ],
+    [
+        'first_name' => 'Светлана',
+        'last_name' => 'Громов',
+    ],
+];
---
+++ tests/codeception/unit/templates/fixtures/authors.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    'first_name' => $faker->firstName,
+    'last_name' => $faker->lastName,
+];
